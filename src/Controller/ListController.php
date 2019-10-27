fcf34ac77a06c1ef23e8df95d6e93b2e5b90263f
Your Name <you@example.com>
2019-10-27 23:47:46 +0700

    commit

---
+++ src/Controller/ListController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+
+class ListController extends AbstractFOSRestController
+{
+    public function getListsAction() {
+
+    }
+
+    public function getListAction(int $id) {
+
+    }
+
+    public function postListsAction() {
+
+    }
+
+    public function getListTasksAction(int $id) {
+
+    }
+
+    public function putListsAction() {
+
+    }
+
+    public function stateListsAction(int $id) {
+
+    }
+}
